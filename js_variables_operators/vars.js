e83d857a92788296c540edd3fb244fa8612cb2a4
JemmUA <jemm.ua@gmail.com>
2025-01-15 19:23:33 +0200

    js variables, operators in profress

--- js_variables_operators/vars.js
+++ js_variables_operators/vars.js
@@ -1,7 +1,7 @@
-const user= "Olexiy";
+const user= "Oleksiy";
 let isAdmin;
 let age= 41;
-
+let acceptedAge = "41";
 
 isAdmin = true;
 console.log(user);
@@ -12,6 +12,8 @@ age+=1;
 console.log("Тип данних 'user':", typeof user);
 console.log("User", user, "is admin:"+" ", isAdmin);
 console.log("Тип данних 'isAdmin':", typeof isAdmin);
+console.log("Тип данних 'age':", typeof age);
+console.log("Тип данних 'acceptedAge':", typeof acceptedAge);
 console.log(age);
 console.log(age++);
 console.log(age);
@@ -19,3 +21,21 @@ console.log(++age);
 console.log(age);
 const userInfo= user + " віком " + age;
 console.log(userInfo);
+
+const numbersArr = [1,2,3];
+numbersArr[5] = 5;
+console.log(numbersArr[4]);
+delete numbersArr[0];
+console.log(numbersArr);
+
+Object.freeze(numbersArr);
+console.log("user.length: ", user.length);
+console.log("-user.length: ", -user.length);
+console.log("operator didn't change variable - user.length:", user.length);
+console.log("Array numbersArr length: ", numbersArr.length);
+
+console.log("acceptedAge  + 11: ", acceptedAge  + 11)
+console.log("+ converts string to number if it possible - +acceptedAge  + 11: ",+acceptedAge + 11)
+console.log("NaN - not a number: +user:", +user);
+console.log("NaN never equals NaN - +user===+user:", +user===+user);
+
