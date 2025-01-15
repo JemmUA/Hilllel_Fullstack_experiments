66d365ec428e583461431fad762e1a7e1dda6496
JemmUA <jemm.ua@gmail.com>
2025-01-15 13:36:17 +0200

    typeof

---
+++ js_variables_operators/vars.js
@@ -0,0 +1,21 @@
+const user= "Olexiy";
+let isAdmin;
+let age= 41;
+
+
+isAdmin = true;
+console.log(user);
+age=age + 1;
+age+=1;
+
+
+console.log("Тип данних 'user':", typeof user);
+console.log("User", user, "is admin:"+" ", isAdmin);
+console.log("Тип данних 'isAdmin':", typeof isAdmin);
+console.log(age);
+console.log(age++);
+console.log(age);
+console.log(++age);
+console.log(age);
+const userInfo= user + " віком " + age;
+console.log(userInfo);
