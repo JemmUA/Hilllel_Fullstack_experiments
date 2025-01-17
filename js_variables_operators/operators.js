8d9efaccac22d4a11c3670e6da0da3a5af722130
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-01-17 15:52:50 +0200

    js add for in, for of

--- js_variables_operators/operators.js
+++ js_variables_operators/operators.js
@@ -1,10 +1,10 @@
-let x = true;
+let x = 0;
 
 if ((x > 0) && (typeof x == "number")) {
-    console.log("x more then 0");
+    console.log("x more than 0");
 } else if (x === 0) {
     console.log("x equals 0");
-} else {
+} else {6
     console.log("x less then 0 or is not a number");
 }
 
@@ -12,4 +12,36 @@ if (typeof x == "number") {
     console.log(`x == ${x} ...Number is a`, typeof x);
 } else {
     console.log(`x = ${x}, is a`, typeof x);
+}
+if (typeof x == "number") {
+    console.log(x > 0 ? "Is more than zero" : "Is not more than zero");
+} else {
+    console.log("Is not a number");
+}
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+}
+
+var obj = {a: 1, b: 2, q: "what else?"};
+for (const prop in obj) {
+    console.log("property",prop + ": " + obj[prop]);
+}
+
+var arr = [10, 20, 30, "numbers"];
+for (const value of arr) {
+    console.log(value); // Виводить 10, 20, 30
+}
+
+outerLoop: for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 5; j++) {
+        if (i === j) {
+            console.log("continue outerLoop")
+            continue outerLoop;
+        } else if ((i === 3) && (j === 2)) {
+            console.log("break outerLoop")
+            break outerLoop;
+        }
+        console.log("i = " + i + ", j = " + j);
+    }
 }
