9a69e3bee92d7e8de5387a7d207e076996912411
JemmUA <jemm.ua@gmail.com>
2025-01-20 19:16:54 +0200

    cycle for with function

--- js/loops/loops.js
+++ js/loops/loops.js
@@ -3,3 +3,13 @@ console.log("Loops!");
 for (let i=0; i<3; console.log(i), i++, console.log(i)) {
     console.log("Loop for, i =",i);
 }
+
+
+for (let i=0; i<3; i++, eatPie()) {
+    console.log("Loop #2, i =",i);
+}
+
+
+function eatPie () {
+    console.log("I eat delicious Pie!");
+}
