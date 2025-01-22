5a9cfa684fc477a16a09387c4c788cc9ec1dab1f
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-01-22 16:31:04 +0200

    callback improoved

--- js/functions/callback.js
+++ js/functions/callback.js
@@ -12,30 +12,30 @@ console.log("Callbacks");
 function makeTea (sugarSpoons) {
   if (sugarSpoons && sugarSpoons > 0) {
     if (sugarSpoons === 1) {
-      console.log(`I made tea for you with ${sugarSpoons} spoon of sugar.`);
+      return `I made tea for you with ${sugarSpoons} spoon of sugar.`;
     } else {
-      console.log(`I made tea for you with ${sugarSpoons} spoons of sugar.`);
+      return `I made tea for you with ${sugarSpoons} spoons of sugar.`;
     }
   } else {
-    console.log(`I made tea for you with no sugar.`);
+    return `I made tea for you with no sugar.`;
   }
 }
 
 function makeCoffee (sugarSpoons) {
   if (sugarSpoons && sugarSpoons > 0) {
     if (sugarSpoons === 1) {
-      console.log(`I made coffee for you with ${sugarSpoons} spoon of sugar.`);
+      return `I made coffee for you with ${sugarSpoons} spoon of sugar.`;
     } else {
-      console.log(`I made coffee for you with ${sugarSpoons} spoons of sugar.`);
+      return `I made coffee for you with ${sugarSpoons} spoons of sugar.`;
     }
   } else {
-    console.log(`I made sugar free coffee for you.`);
+    return `I made sugar free coffee for you.`;
   }
 }
 
 function cook (cb, sugarAmount) {
-  cb(sugarAmount)
-  console.log("Bon appetit!");
+
+  console.log(cb(sugarAmount), "Bon appetit!");
 }
 
 cook(makeTea, 3);
