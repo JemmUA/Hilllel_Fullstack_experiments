72bb3a65cfa751b356ffbd900b59294b061405a8
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-01-30 00:51:50 +0200

    closures. call, apply, bind

--- js/functions/closures/closures.js
+++ js/functions/closures/closures.js
@@ -28,3 +28,32 @@ const thirdRes = function() {
 }();
 
 thirdRes();
+
+console.log("Казус із замиканням \"closures\"")
+function makeThreeSummons() {
+  for (var i = 0; i <= 2; i++) { // let вирішує проблему
+    const showMe =function() {
+      console.log(i);
+    }
+    setTimeout(showMe, 1000);
+  }
+  console.log("var виходить зі свого скопу і запам'ятовує тут і =", i); //var винесе 3 аж сюди і замикання дістане трійку звідси
+  console.log("Case with closure")
+}
+makeThreeSummons();
+
+
+// Функції call(), bind(), apply()
+// function.call(object); // Одноразовий виклик функції до об'єкту без прив'язки
+// якщо треба передати параметри, то передаємо через кому(скільки завгодно): function.call(object, parameter, parameter2);
+// apply робить те ж саме, тільки параметри передає масивом: function.apply(object, [parameter, parameter2]);
+
+// bind() робить нову функцію, де function назавжди прив'язана до object: const newFunction = function.bind(object);
+// Через кому можна передати параметри const newFunction = function.bind(object, parameter1, parameter2, parameter3);
+// Параметри прив'язуються.
+// bind() вирішує проблему з втратою контексту. Додаткові параметри можна додати при виклиціє
+// Виклик newFunction();
+// setTimeout(object.method, 1000); // втрачає контекст, бо втрачає this
+// setTimeout(method.bind(object, parameter, parameter2);
+
+// У стрілочних функцій немає this, вони не мають своєї області видимості.
