35ce5f643cd7e875d814d1250ee4cdbdaa3e6148
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-01-30 15:16:30 +0200

    closures, call, apply, bind

--- js/functions/closures/closures.js
+++ js/functions/closures/closures.js
@@ -35,12 +35,12 @@ function makeThreeSummons() {
     const showMe =function() {
       console.log(i);
     }
-    setTimeout(showMe, 1000);
+    setTimeout(showMe, 100);
   }
   console.log("var виходить зі свого скопу і запам'ятовує тут і =", i); //var винесе 3 аж сюди і замикання дістане трійку звідси
   console.log("Case with closure")
 }
-makeThreeSummons();
+// makeThreeSummons();
 
 
 // Функції call(), bind(), apply()
@@ -51,9 +51,44 @@ makeThreeSummons();
 // bind() робить нову функцію, де function назавжди прив'язана до object: const newFunction = function.bind(object);
 // Через кому можна передати параметри const newFunction = function.bind(object, parameter1, parameter2, parameter3);
 // Параметри прив'язуються.
-// bind() вирішує проблему з втратою контексту. Додаткові параметри можна додати при виклиціє
+// bind() вирішує проблему з втратою контексту. Додаткові параметри можна додати при виклиці
 // Виклик newFunction();
 // setTimeout(object.method, 1000); // втрачає контекст, бо втрачає this
 // setTimeout(method.bind(object, parameter, parameter2);
 
 // У стрілочних функцій немає this, вони не мають своєї області видимості.
+
+const list = [12, 23, 100, 34, 56, 9, 233]
+const myMax = (l) => {
+  let maxValue = l[0];
+  let maxOfTwo;
+  // let maxOfTwo = (Math.max).apply(l, [f, s]);
+  // console.log("Math.max.apply(l)", maxOfTwo);
+  // console.log(l);
+  for (let i = 0; i < l.length - 1; i++) {
+    maxOfTwo = Math.max.apply(l, [l[i], l[i + 1]]);
+    // console.log("maxOfTwo:", maxOfTwo);
+    // maxValue = maxValue > Math.max(l[i],l[i + 1]) ? maxValue : maxValue = Math.max(l[i],l[i + 1]);
+    maxValue = maxValue > maxOfTwo ? maxValue : maxValue = maxOfTwo;
+    // console.log(maxValue);
+  }
+  return maxValue;
+}
+console.log("my max:",myMax(list)); // 233
+
+
+const myMul = (a, b) => a * b;
+
+const multObj = {
+  multDouble : 2,
+  multTriple : 3
+}
+const myBindDouble = myMul.bind(multObj, multObj.multDouble);
+console.log("My double:",myBindDouble(11));
+
+const trpl = {
+  mul : 3
+}
+const myBindTriple = myMul.bind(multObj, multObj.multTriple);
+// const myBindTriple = myMul.bind(multObj, 3);
+console.log("My triple:",myBindTriple(33));
