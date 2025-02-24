3e6c9ae089c8b92015d01f30b398f216e4484dc7
JemmUA <jemm.ua@gmail.com>
2025-02-24 12:08:52 +0200

    toggle is solved

--- dom_bom/toggle/toggle.js
+++ dom_bom/toggle/toggle.js
@@ -4,9 +4,10 @@ console.log(button);
 
 function toggle() {
     console.log("крутяк");
-    const divchik = document.querySelector(".green");
+    const divchik = document.getElementById("switcher");
     console.log(divchik.classList);
     divchik.classList.toggle("green");
     divchik.classList.toggle("red");
+    console.log("toggle green, toggle red")
     console.log(divchik.classList);
 }
