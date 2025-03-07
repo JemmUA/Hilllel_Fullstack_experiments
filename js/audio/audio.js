ef29f1f63c4074c2e225122e1a0b3a6eb3d09889
JemmUA <jemm.ua@gmail.com>
2025-03-07 18:38:37 +0200

    promise, fetch

--- js/audio/audio.js
+++ js/audio/audio.js
@@ -1,6 +1,9 @@
 // from stackoverflow
+// 1.
 // let audio = new Audio('audio_file.mp3');
 // audio.play();
+// 2.
+// document.getElementById('yourAudioTag').play();
 
 const container = document.getElementById("container");
 const buttonPlay = document.createElement("button");
@@ -8,7 +11,7 @@ let pourBeerSound = new Audio('sound/pouring_beer.mp3');
 let openCanSound = new Audio('sound/beer-can-opening.mp3');
 buttonPlay.addEventListener("click", () => {
     console.log("some beer?");
-    // openCanSound.play();
+    openCanSound.play();
     pourBeerSound.play();
 });
 buttonPlay.innerText = "Play sound";
---
+++ js/functions/callback_and_promise/callback_and_promise.js
@@ -0,0 +1,74 @@
+console.log("Callback and Promise");
+
+function waitAndRun(ms, callback) {
+    console.log('Таймер стартував');
+    setTimeout(() => {
+        console.log('Таймер завершився');
+        callback();
+    }, ms);
+}
+
+waitAndRun(2000, () => {
+    console.log('Callback виконано');
+});
+
+// Callback hell
+setTimeout(() => {
+    console.log('Перший таймер завершився');
+    setTimeout(() => {
+        console.log('Другий таймер завершився');
+        setTimeout(() => {
+            console.log('Третій таймер завершився');
+        }, 1000);
+    }, 1000);
+}, 1000);
+
+
+
+// Promise
+const loadData = new Promise((resolve, reject) => {
+    console.log('Завантаження даних почалося');
+    setTimeout(() => {
+        // Симулюємо успішне завершення асинхронної операції
+        resolve('Дані успішно завантажені');
+    }, 2000);
+});
+
+loadData.then((message) => {
+    console.log(message); // Виведе "Дані успішно завантажені" після 2 секунд
+}).catch((error) => {
+    console.error(error); // Виведе помилку, якщо операція буде відхилена
+}).finally(() => {
+    console.log('Операція завершена'); // Виконається після завершення Promise, незалежно від результату
+});
+
+// XHR
+const xhr = new XMLHttpRequest(); // Створення нового екземпляра XHR
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/1', true); // Налаштування запиту
+
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Перевірка статусу відповіді
+        console.log('Відповідь отримано:', xhr.responseText); // Обробка відповіді від сервера
+    } else {
+        console.error('Запит не вдався:', xhr.statusText); // Обробка помилки запиту
+    }
+};
+
+xhr.onerror = function() {
+    console.error('Помилка запиту'); // Обробка помилки мережі
+};
+
+xhr.send(); // Відправлення запиту
+
+// Fetch
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Запит не вдався: ' + response.statusText);
+        }
+        return response.json(); // Парсинг тіла відповіді як JSON
+    })
+    .then(data => console.log(data)) // Обробка отриманих даних
+    .catch(error => console.error('Помилка:', error)); // Обробка помилок запиту
+
