61a5bfd06bfcc2030bd81f48667803318f9dd764
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-06-24 20:17:46 +0300

    node path

--- npm/path/path__.mjs
+++ npm/path/path__.mjs
@@ -1,6 +1,11 @@
 import path from 'node:path';
 
 const filePath = "./data/file.txt"
+console.log("===============================");
+console.log("meta", import.meta)
+console.log("===============================");
 const fileName= import.meta.url;
+const absolutePath = path.resolve(fileName, filePath);
 console.log("File name: ", fileName);
 console.log("File path: ", filePath);
+console.log("Absolute path: ", absolutePath);
