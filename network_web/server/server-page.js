dd968a47a174281a37956a068c145c184998c979
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-06-24 23:13:16 +0300

    socket

---
+++ network_web/server/server-page.js
@@ -0,0 +1,16 @@
+export const serverPageHtml = '<!DOCTYPE html>\n' +
+  '<html lang="en">\n' +
+  '<head>\n' +
+  '  <meta charset="UTF-8">\n' +
+  '  <title>Server http</title>\n' +
+  ' <style> ' +
+  '  body {' +
+  ' background: #117733; ' +
+  '}' +
+  '</style> '+
+  '</head>\n' +
+  '<body>\n' +
+  '  <h1> Server with true html</h1>\n' +
+  '  <h2>Glad to meet you!</h2>\n' +
+  '</body>\n' +
+  '</html>';
--- network_web/server/server.js
+++ network_web/server/server.js
@@ -1,13 +1,36 @@
 import http from "node:http";
+import {serverPageHtml} from "./server-page.js";
 
 const PORT = 3050;
 
 const server = http.createServer((request, response)=> {
   response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end(`Hello from Jemm, server.js! Port: ${PORT}`)
+  // response.setHeader('Content-Type', 'text/plain; charset=UTF-8');
+  response.setHeader('Content-Type', 'text/html; charset=UTF-8');
+  response.write("Write!");
+  response.write('<h1>Hello, my server )))</h1>');
+
+  // console.log(request.url); // request's url
+
+  // if (request.url === '/css') {
+  //   response.setHeader ('Content-Type', 'text/css');
+  //   response.write( '' +
+  //     '      body {\n' +
+  //     '      background-color: lightblue;\n' +
+  //     '    }' +
+  //     '    ');
+  //   response.end();
+  //   return;
+  // }
+
+  response.write('Хе');
+  response.write(serverPageHtml);
+
+  // response.end(`Ку-ку :))) Hello from Jemm, server.js! Port: ${PORT}`); // End of response - final operation.
+  response.end(); // Empty end if we write.
 });
 
+// Starting the server
 server.listen( PORT, () => {
   console.log(`Yessss!!! Server started on port ${PORT}`);
 });
