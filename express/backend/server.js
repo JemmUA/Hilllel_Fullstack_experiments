618ee8c7d00643cf9ef06f7e2ceb06538e2853bc
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-06-28 12:37:54 +0300

    express00

---
+++ express/backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const PORT = 3003;
+const app = express();
+
+const annoyingUser = (request, response) => {
+  response.send('You again? Well, hello ;))');
+};
+
+const userInfo = (request, response) => {
+  response.send(`<h2>User ID: ${request.params.id}</h2>`);
+};
+
+app.get('/', (request, response) => {
+  response.status(200).send('Hello from Express server!');
+});
+
+app.get('/rerequests', annoyingUser);
+app.get('/users/:id', userInfo);
+
+
+app.listen(PORT, () => {
+  console.log(`Express server has started at port: ${PORT}`);
+});
